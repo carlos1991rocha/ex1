8c5f7bdb4ef9fdff41c7ddcbb4a25085bc3a20c8
Carlos Alberto Rocha <carlos1991rocha.charles@gmail.com>
2019-12-25 18:46:29 -0300

    Priveira versão do código Exercicio Spring Boot + CRUD

---
+++ src/main/java/com/ex1/ex1/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.ex1.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
